289d8410908c4a3dddd51e5259910e9c02ad44c6
Aumnuay Niranartkul <man45045150@hotmail.co.th>
2019-10-10 17:34:47 +0700

    Init

---
+++ public/src/js/app.js
@@ -0,0 +1,71 @@
+
+var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+function displayConfirmNotification(){
+  var options = {
+    body: 'You successfully subscribed to our Notification service!',
+    icon: '/src/images/icons/app-icon-96x96.png',
+    images: '/src/images/sf-boat.jpg',
+    dir : 'ltr',
+    lang : 'en-US',
+    vibrate: [100,50,200],
+    badge : '/src/images/icons/app-icon-96x96.png',
+    tag : 'confirm-notification',
+    renotify : true,
+    data : 'test',
+    action : [
+      { action : 'comfirm' , title : 'Okay',icon:'/src/images/icons/app-icon-96x96.png' },
+      { action : 'cancel' , title : 'Cancel',icon:'/src/images/icons/app-icon-96x96.png' }
+    ]
+  };
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.ready
+      .then(function(swreg){
+        console.log('swreg',swreg);
+        swreg.showNotification('Successfully subscribed(SW)',options);
+      });
+  }
+  
+  // new Notification('Successfully subscribed',options);
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function(result) {
+    console.log('User Choice', result);
+    if (result !== 'granted') {
+      console.log('No notification permission granted!');
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ('Notification' in window) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+  }
+}
---
+++ public/sw.js
@@ -0,0 +1,90 @@
+self.addEventListener('install',function(event){
+  console.log('[Service Worker] Installing Service Worker ...',event);
+});
+self.addEventListener('activate',function(event){
+  console.log('[Service Worker] Activating Service Worker ...',event);
+  return  self.clients.claim();
+});
+// self.addEventListener('fetch',function(event){
+//   console.log('[Service Worker] Fetching Service Worker ...',event);
+//   event.respondWith(fetch( event.request));
+// });
+
+// self.addEventListener('notificationclick', function(event) {
+//   console.log(event.notification.data);
+  
+//   // console.log('notificationclick');
+//   // clients.openWindow("https://www.khaosod.co.th/entertainment/news_2961666?utm_source=izooto&utm_medium=push_notifications&utm_campaign=campaign_entertainment&utm_content=entertainment&utm_term=");
+//   var found = false;
+//   clients.matchAll().then(function(clientsArr) {
+//     for (i = 0; i < clientsArr.length; i++) {
+//       if (clientsArr[i].url === 'http://localhost:3000/index.html') {
+//         // We already have a window to use, focus it.
+//         found = true;
+//         // clientsArr[i].focus();
+//         clientsArr[i].navigate('https://notifyjs.jpillora.com').isTabActive = true;
+//         // clientsArr[i].isTabActive = true;
+//         // self.isTabActive = true;
+//         window.focus();
+//         break;
+//       }
+//     }
+//     if (!found) {
+//       // Create a new window.
+//       var test = 'https://www.khaosod.co.th/entertainment/news_2961666?utm_source=izooto&utm_medium=push_notifications&utm_campaign=campaign_entertainment&utm_content=entertainment&utm_term=';
+//       clients.openWindow(test).then(function(windowClient) {
+//         // do something with the windowClient.
+//       });
+//     }
+//     console.log('found:',found);
+//   });
+// }
+// ,false);
+
+
+addEventListener('notificationclick', event => {
+  event.waitUntil(async function() {
+    const allClients = await clients.matchAll({
+      includeUncontrolled: true
+    });
+
+    let chatClient;
+    let host = 'localhost:3000';
+
+    // Let's see if we already have a chat window open:
+    for (const client of allClients) {
+      const url = new URL(client.url);
+      console.log(url);
+      if (url.host == host) {
+        // Excellent, let's use it!
+        client.focus();
+        chatClient = client;
+        break;
+      }
+    }
+
+    // If we didn't find an existing chat window,
+    // open a new one:
+    if (!chatClient) {
+      chatClient = await clients.openWindow('/chat/');
+    }
+
+    // Message the client:
+    chatClient.postMessage("New chat messages!");
+  }());
+});
+
+
+
+self.addEventListener('DOMContentLoaded', function() {
+  if (!Notification) {
+   alert('Desktop notifications not available in your browser. Try Chromium.');
+   return;
+  }
+ 
+  if (Notification.permission !== 'granted')
+   Notification.requestPermission();
+  else{
+    console.log('granted');
+  }
+ });
