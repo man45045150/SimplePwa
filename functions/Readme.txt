6dacce02cbad353bd5c3ee11286e875d842ef215
Aumnuay Niranartkul <man45045150@hotmail.co.th>
2019-10-13 17:00:56 +0700

    update

---
+++ functions/Readme.txt
@@ -0,0 +1,19 @@
+npm run web-push generate-vapid-keys
+
+
+Public Key:
+BKSZxlrZqLEHcwz4kFcf20Cim8oSijs_qzUyDEXFuUuIhjc_4oPZ-pCbthE2_nZuhWEWx2sSPyNnLOK_uk5jzzo
+
+// for server
+Private Key:
+4e1j7i-SRjWLmDqi7J801Xe9PMGrDdM8jku9INZ0FFo
+
+
+{ 
+   "endpoint":"https://fcm.googleapis.com/fcm/send/dB_fhGT-Jzk:APA91bGD8hmBh3PSD0yLy-ZZrUZ_IyO4AQimv2shdkgeaZHkQr-nsSIGV1hWaBU-UwXMFDZG9SNsfRRsF7XbbJszAUM8P1J8HPfIMIt8vRz_HB2DPtbnME_b15zKUen8Hx6quzirwhJI",
+   "expirationTime":null,
+   "keys":{ 
+      "p256dh":"BMPyZg2_dLZG8pabZuRv_9yLvdk_Hn6qgPL45-japbVRJotUakdKQyzzxx3WS6VXgKusk5yEfMiANl0W3M_Xz5s",
+      "auth":"Zq3SKpTp6ZlvFEI5xVRLdg"
+   }
+}
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -51,18 +51,67 @@ function displayConfirmNotification(){
   // new Notification('Successfully subscribed',options);
 }
 
+function configurePushSub(){
+  if(!('serviceWorker' in navigator))
+    return;
+  
+  var reg;
+  navigator.serviceWorker.ready
+    .then(function(swreg){
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function(sub){
+      if( sub === null){
+        // Create a new subscription
+        // Protect by VAPID
+        var vapidPublicKey = 'BKSZxlrZqLEHcwz4kFcf20Cim8oSijs_qzUyDEXFuUuIhjc_4oPZ-pCbthE2_nZuhWEWx2sSPyNnLOK_uk5jzzo';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly : true,
+          applicationServerKey: convertedVapidPublicKey
+        });
+      }else{
+        // We have a subscription
+
+      }
+    })
+    .then(function(newSub){
+      const myHeaders = new Headers();
+      myHeaders.append('Content-Type', 'text/plain');
+      myHeaders.append('Accept', 'application/json, text/plain, */*');
+    
+    return fetch('https://localhost:5001/api/messages/subscription', {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(newSub),
+        mode : 'no-cors',
+        credentials: 'same-origin'
+      })
+    })
+    .then(function(res) {
+      if (res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 function askForNotificationPermission() {
   Notification.requestPermission(function(result) {
     console.log('User Choice', result);
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+      configurePushSub();
+      // displayConfirmNotification();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
